c4d58d2d4e790fb13fb30e1d467884496f147267
Kevin Eliezer <kevineli.gp@gmail.com>
2020-10-23 01:40:32 +0100

    Tailwind Components for project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+
+      spacing: {
+        '80': '20rem',
+        '88': '22rem',
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
+  }
+}
